93405feb7d7166e7c75f6b4dc87e23423fdc38af
igor-ht <i_ht93@hotmail.com>
2023-09-07 18:44:46 +0300

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,11 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { loadEnv } from 'vite';
 
-const { SITE_URL } = loadEnv(process.env.SITE_URL, process.cwd(), '');
+const { SITE_URL, PRODUCTION } = loadEnv(process.env, process.cwd(), '');
 
 export default defineConfig({
 	// all the usual config goes here...
-	site: SITE_URL || 'www.example.com',
+	site: PRODUCTION ? SITE_URL : 'https://www.example.com',
 	integrations: [mdx(), sitemap()],
 	vite: {
 		css: {
