291d493b87757cb6c71660f306b1ab476fa0b039
igor-ht <i_ht93@hotmail.com>
2023-09-07 17:07:38 +0300

    scss @use alias bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,21 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import { loadEnv } from 'vite';
 
-import react from '@astrojs/react';
+const { SITE_URL } = loadEnv(process.env.SITE_URL, process.cwd(), '');
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://iht-blog.app.vercel',
-	integrations: [mdx(), sitemap(), react()],
+	// all the usual config goes here...
+	site: SITE_URL,
+	integrations: [mdx(), sitemap()],
+	vite: {
+		css: {
+			preprocessorOptions: {
+				scss: {
+					additionalData: `@use '@/styles/_main.scss' as *;`,
+				},
+			},
+		},
+	},
 });
