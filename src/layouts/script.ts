24afc0153b6bba6041cd90bf4b75a2ca77b59dc6
igor-ht <i_ht93@hotmail.com>
2023-09-10 12:32:49 +0300

    toggleTheme feature done

--- src/layouts/script.ts
+++ src/layouts/script.ts
@@ -1,14 +1,28 @@
 const setTheme = () => {
-	console.log('document astro:afte-swap event triggered');
-	let currentTheme;
-	if (typeof localStorage !== 'undefined') {
-		currentTheme = localStorage.getItem('theme');
+	let currentTheme = document.documentElement.getAttribute('data-theme') as string;
+	if (window.matchMedia('(prefers-color-scheme: dark)').matches) currentTheme = 'dark';
+	if (typeof localStorage !== 'undefined' && localStorage.getItem('theme')) currentTheme = localStorage.getItem('theme') as string;
+	window.localStorage.setItem('theme', currentTheme);
+	document.documentElement.setAttribute('data-theme', currentTheme);
+};
+setTheme();
+
+const handleToggleTheme = () => {
+	const element = document.documentElement;
+	const currentTheme = element.getAttribute('data-theme');
+	if (currentTheme === 'light') {
+		element.setAttribute('data-theme', 'dark');
+		localStorage.setItem('theme', 'dark');
+	} else {
+		element.setAttribute('data-theme', 'light');
+		localStorage.setItem('theme', 'light');
 	}
-	if (currentTheme === 'dark') {
-		document.documentElement.setAttribute('data-theme', 'dark');
-	} else document.documentElement.setAttribute('data-theme', 'light');
 };
 
-setTheme();
-// Runs on view transitions navigation
-document.addEventListener('astro:after-swap', setTheme);
+const AstroAfterSwapHandler = () => {
+	setTheme();
+	document.querySelector('#themeToggle')?.addEventListener('click', handleToggleTheme);
+};
+
+document.querySelector('#themeToggle')?.addEventListener('click', handleToggleTheme);
+document.addEventListener('astro:after-swap', AstroAfterSwapHandler);
