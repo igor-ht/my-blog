2c842e32dac751ca6fc678f0e0a30432edffa11d
igor-ht <i_ht93@hotmail.com>
2023-09-13 18:24:53 +0300

    vercel ssr integration add for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel/serverless';
 import { loadEnv } from 'vite';
 
 const { SITE_URL, PRODUCTION } = loadEnv(process.env, process.cwd(), '');
 
+// https://astro.build/config
 export default defineConfig({
 	// all the usual config goes here...
-	site: PRODUCTION ? SITE_URL : 'https://www.example.com',
+	site: PRODUCTION ? SITE_URL : 'http://localhost:3000/',
 	integrations: [sitemap()],
 	vite: {
 		css: {
@@ -17,4 +19,6 @@ export default defineConfig({
 			},
 		},
 	},
+	output: 'server',
+	adapter: vercel(),
 });
